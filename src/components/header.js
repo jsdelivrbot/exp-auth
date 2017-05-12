5a451fdf94f744e4ee8b63781952755bf8033f58
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-05-12 18:33:01 -0300

    improved header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
 
 class Header extends Component {
+  renderLinks(){
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link to="/signout">Sign Out</Link>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <NavLink to="/signin" activeClassName="selected">Sign In</NavLink>
+        </li>,
+        <li className="nav-item" key={2}>
+          <NavLink to="/signup" activeClassName="selected">Sign Up</NavLink>
+        </li>
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
-          <li className="nav-item">Sign In</li>
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect (mapStateToProps, null)(Header);
