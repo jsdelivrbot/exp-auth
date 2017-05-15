5625e91856cb58e10b39703a602bcfcf38ad3169
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-05-15 17:26:07 -0300

    user can refresh page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 import Header from './components/header';
 import RequireAuth from './components/auth/require_auth';
@@ -15,9 +16,16 @@ import SignUp from './components/auth/signup';
 import Dashboard from './components/dashboard';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a token, consider the user authenticated
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <div>
         <Header />
